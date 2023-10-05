37f9d7297aeebeb021a80367b77e73658d5bddc7
sanabarakat <sbarakat.ieu2021@student.ie.edu>
2023-10-05 23:19:35 +0200

    homepage

--- src/components/hp.vue
+++ src/components/hp.vue
@@ -1,7 +1,29 @@
 <template>
     <div>
-        <header>
-            <h1>IE Bank</h1>
-        </header>
+      <h1>IE Bank</h1>
+      <p>Your Trusted Banking Partner</p>
+      <img src="download.jpg" alt="IE Tower">
+      <br>
+      <button>View Accounts</button>
     </div>
-</template>
+  </template>
+  
+
+  
+  <style scoped>
+  div {
+    background-color: rgba(11, 162, 217, 0.623);
+    padding: 20px;  
+    text-align: center;
+  }
+  h1 {
+    color: rgb(7, 7, 138);
+  }
+  p {
+    font-size: 1.2rem;
+  }
+
+  img {
+  max-width: 100%; 
+}
+  </style>
